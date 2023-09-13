ef41f38a1914ae66cbb0808b8c62fe30747208b4
Netology <viltis.gfx@gmail.com>
2023-09-13 14:35:54 +0300

    refactor3

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,19 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+
         double ticketPrice = 13676;
+
         int rublesPerMile = 20;
+
         int miles = (int) (ticketPrice / rublesPerMile);
-        boolean hasDecimalPart = ticketPrice % rublesPerMile != 0;
+
+        boolean hasDecimalPart = (ticketPrice % rublesPerMile) != 0;
+
         if (hasDecimalPart) {
-            miles++;
+            miles--;
         }
+        
         System.out.println("Количество начисленных миль: " + miles);
     }
 }
